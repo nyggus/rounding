4c7fca14a7a63589e7e0ccc299e95845b98736eb
Marcin Kozak <nyggus@gmail.com>
2022-09-28 08:59:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extras_requirements = {
 
 setuptools.setup(
     name="rounder",
-    version="0.6.3",
+    version="0.6.4",
     author="Ruud van der Ham & Nyggus",
     author_email="nyggus@gmail.com",
     description="A tool for rounding numbers in complex Python objects",
